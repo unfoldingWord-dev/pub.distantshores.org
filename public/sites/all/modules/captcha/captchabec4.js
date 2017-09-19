8b170cd5d25c526fef9426a39504cd28b9949f37
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2017-09-19 17:49:23 -0400

    WIP

---
+++ public/sites/all/modules/captcha/captchabec4.js
@@ -0,0 +1,2 @@
+(function($){Drupal.behaviors.captcha={attach:function(context){$("#edit-captcha-response").attr("autocomplete","off");}};Drupal.behaviors.captchaAdmin={attach:function(context){$("#edit-captcha-add-captcha-description").click(function(){if($("#edit-captcha-add-captcha-description").is(":checked")){$("div.form-item-captcha-description").show('slow');}
+else{$("div.form-item-captcha-description").hide('slow');}});if(!$("#edit-captcha-add-captcha-description").is(":checked")){$("div.form-item-captcha-description").hide();}}};})(jQuery);
